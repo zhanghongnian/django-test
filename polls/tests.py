08d45e3259d347081e22c83b8ce8f88421bf788d
zhnliving <zhnliving@icloud.com>
2020-05-24 07:52:31 +0800

    polls detail view 测试用例

--- polls/tests.py
+++ polls/tests.py
@@ -68,3 +68,17 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+class QuestDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
