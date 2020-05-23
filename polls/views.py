5fe876349a920fd3dc9657f2a4ddaeb6d55156e6
zhnliving <zhnliving@icloud.com>
2020-05-23 20:26:12 +0800

    优化 polls view

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*- 
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'lastest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     response = "You're looking at question %s."
