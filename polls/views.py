ffd7f84ef9ccb09b0e6d15faa7bba321ad926dd5
zhnliving <zhnliving@icloud.com>
2020-05-23 20:29:19 +0800

    使用 render 简化代码

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*- 
 
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'lastest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     response = "You're looking at question %s."
