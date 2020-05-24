e66fb1e8454c2fb41c9b39c35d93480990f6f7f6
zhnliving <zhnliving@icloud.com>
2020-05-24 08:27:13 +0800

    表单字段分组

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,13 @@
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}), 
+        ('Date information', {'fields': ['pub_date']})
+    ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
