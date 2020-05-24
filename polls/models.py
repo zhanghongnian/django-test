50d0c5577c0d19ca925ce8db90d552b64fac79cf
zhnliving <zhnliving@icloud.com>
2020-05-24 11:37:10 +0800

    优化 was_published_recently 显示

--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1) and self.pub_date <= timezone.now()
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, db_constraint=False, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
