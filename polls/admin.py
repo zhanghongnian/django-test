ec5b72afa400a667bc14b3eabf8b71289c2988d3
zhnliving <zhnliving@icloud.com>
2020-05-24 08:23:01 +0800

    polls 使用 QuestionAdmin 控制 form 字段顺序

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 
 from .models import Question
 
-# Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
